a4eb2139dffb8ff699ee34be2165de15a4e28787
Tammy Dam <tdam@terpmail.umd.edu>
2020-05-01 23:34:04 -0400

    php - tammy

---
+++ page.php
@@ -0,0 +1,8 @@
+get_header();
+
+<div>
+    <div class = "main container">
+        <div class = "tabs">
+            <ul>
+                <li class = "is-active"><a>Pictures</a></li>
+                <li><a>Home>
