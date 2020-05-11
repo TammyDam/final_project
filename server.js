39b953c34f3f04b43b5fb5527ac6cea9d9153d3a
tanishq-kaushik <60356305+tanishq-kaushik@users.noreply.github.com>
2020-05-11 16:56:57 -0400

    Working Server.js

--- server.js
+++ server.js
@@ -50,20 +50,9 @@ app.route("/api")
       })
       .catch((err) => {
         console.log(err);
-      })
-      .put((req, res)) => {
-        console.log(put)
-      }
+      });
     }
-    
-    /*.put((req, res))=> {
-      console.log("/api put request", req.body);
-        if(err) {
-          return console.log(err.message); 
-        }
-        res.json({result:"Your form has been stored"});
-  });*/
-
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
